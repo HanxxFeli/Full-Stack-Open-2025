835c856ffd78cea33b6968312627604dede40ef7
Hans Feliciano <hgvfeliciano10@gmail.com>
2025-07-28 22:11:05 -0600

    Initialize folder for Unicafe exercsies

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+const App = () => { 
+  // save clicks of each button to its own state 
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return ( 
+    <div>
+      Code to be written
+    </div>
+  )
+}
+
+// good 
+
+// neutral 
+
+//bad 
+
+// all / total 
+
+// average
+
+// percentage of positive feedback (good/total)
+
+
+export default App
+
