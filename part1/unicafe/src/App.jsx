d6b3c07e5ab2867c43ec2097213a489b8d58d67e
Hans Feliciano <hgvfeliciano10@gmail.com>
2025-07-30 21:57:57 -0600

    solved exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,8 +1,23 @@
 import { useState } from 'react'
 
+// button component
 const Button = ({onClick, text}) => { 
   return ( 
     <button onClick={onClick}> {text}</button>
+  )
+}
+
+// statistics component
+const Statistics = (props) => { 
+  const total = props.good + props.neutral + props.bad
+
+  return ( 
+    <div> 
+      <h1>statistics</h1>
+      <p>All {total}</p>
+      <p>Average {((props.good * 1) + (props.neutral * 0) + (props.bad * -1))/total}</p>
+      <p>Positive {props.good / total}</p>
+    </div>
   )
 }
 
@@ -11,13 +26,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total = good + neutral + bad
-  const average = ((good * 1) + (neutral * 0) + (bad * -1))/total
-  const positive = (good / total) * 100
 
   // create the handlers for each button
   const handleGoodClick = () => {
-    console.log('average before', average) 
 
     // create the variable for the updated value 
     const updatedGood = good + 1
@@ -44,15 +55,12 @@ const App = () => {
       <Button onClick={handleGoodClick} text='good'/>
       <Button onClick={handleNeutralClick} text='neutral'/>
       <Button onClick={handleBadClick} text='bad'/>
-      <h1>statistics</h1>
       <div>
         <p>Good {good}</p>
         <p>Neutral {neutral}</p>
         <p>Bad {bad}</p>
-        <p>All {total}</p>
-        <p>Average {average}</p>
-        <p>positive {positive} %</p>
       </div>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
