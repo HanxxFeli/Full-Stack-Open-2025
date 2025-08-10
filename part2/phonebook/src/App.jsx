3fa1f6fd99ad72917e85dc76980a98f7c9a34cb2
Hans Feliciano <hgvfeliciano10@gmail.com>
2025-08-09 23:58:18 -0600

    solved exercises 2.11. Used axios to fetch data from server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios'
 import Person from './components/Person'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 
 const App = () => { 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [filteredPersons, setFilteredPersons] = useState([])
   const [newName, setNewName] = useState('') // controlling the form input element
   const [newNumber, setNewNumber] = useState('')
   const [filter, setNewFilter] = useState('') // for filtering people by name
+
+  // fetching data from the server using axios 
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response=> {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   // adding a new contact 
   const addContact = (event) => { 
